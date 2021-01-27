19f499eb2d0264d9b8f62e30285d48dbeafaca67
Caio Ramos <caiocrr@poli.ufrj.br>
2021-01-27 02:43:14 -0300

    guloso simples

---
+++ main.js
@@ -0,0 +1,254 @@
+
+// Casa -> Casa: 0m
+// Casa -> Guanabara Tijuca (Almirante Cochrane): 38m
+// Casa -> Prezunic Olaria: 58m
+// Casa -> Guanabara Bonsucesso: 59m
+// Casa -> Mundial Ramos: 52m
+// Casa -> Mundial Tijuca: 36m
+// Casa -> Extra Copacabana: 4m
+// Casa -> Mundial Copacabana: 4m
+// Casa -> Assaí São Cristovao: 59m
+// Casa -> Atacadão Vicente de Carvalho: 52m
+// Casa -> Guanabara Barra da Tijuca: 54m
+// [0,38,58,59,52,36,4,4,59,52,54]
+
+// Guanabara Tijuca (Almirante Cochrane) -> Casa: 38m
+// Guanabara Tijuca (Almirante Cochrane) -> Guanabara Tijuca (Almirante Cochrane): 0m
+// Guanabara Tijuca (Almirante Cochrane) -> Prezunic Olaria: 43m
+// Guanabara Tijuca (Almirante Cochrane) -> Guanabara Bonsucesso: 54m
+// Guanabara Tijuca (Almirante Cochrane) -> Mundial Ramos: 38m
+// Guanabara Tijuca (Almirante Cochrane) -> Mundial Tijuca: 4m
+// Guanabara Tijuca (Almirante Cochrane) -> Extra Copacabana: 35m
+// Guanabara Tijuca (Almirante Cochrane) -> Mundial Copacabana: 37m
+// Guanabara Tijuca (Almirante Cochrane) -> Assaí São Cristovao: 47m
+// Guanabara Tijuca (Almirante Cochrane) -> Atacadão Vicente de Carvalho: 42m
+// Guanabara Tijuca (Almirante Cochrane) -> Guanabara Barra da Tijuca: 1h22m
+// [38,0,43,54,38,4,35,37,47,42,82]
+
+// Prezunic Olaria -> Casa: 57m
+// Prezunic Olaria -> Guanabara Tijuca (Almirante Cochrane): 42m
+// Prezunic Olaria -> Prezunic Olaria: 0m
+// Prezunic Olaria -> Guanabara Bonsucesso: 18m
+// Prezunic Olaria -> Mundial Ramos: 5m
+// Prezunic Olaria -> Mundial Tijuca: 45m
+// Prezunic Olaria -> Extra Copacabana: 55m
+// Prezunic Olaria -> Mundial Copacabana: 56m
+// Prezunic Olaria -> Assaí São Cristovao: 40m
+// Prezunic Olaria -> Atacadão Vicente de Carvalho: 41m
+// Prezunic Olaria -> Guanabara Barra da Tijuca: 1h43m
+// [57,42,0,18,5,45,55,56,40,41,103]
+
+// Guanabara Bonsucesso -> Casa: 1h
+// Guanabara Bonsucesso -> Guanabara Tijuca (Almirante Cochrane): 56m
+// Guanabara Bonsucesso -> Prezunic Olaria: 17m
+// Guanabara Bonsucesso -> Guanabara Bonsucesso: 0m
+// Guanabara Bonsucesso -> Mundial Ramos: 13m
+// Guanabara Bonsucesso -> Mundial Tijuca: 57m
+// Guanabara Bonsucesso -> Extra Copacabana: 58m
+// Guanabara Bonsucesso -> Mundial Copacabana: 59m
+// Guanabara Bonsucesso -> Assaí São Cristovao: 29m
+// Guanabara Bonsucesso -> Atacadão Vicente de Carvalho: 42m
+// Guanabara Bonsucesso -> Guanabara Barra da Tijuca: 1h37m
+// [60,56,17,0,13,57,58,59,29,42,97]
+
+// Mundial Ramos -> Casa: 53m
+// Mundial Ramos -> Guanabara Tijuca (Almirante Cochrane): 40m
+// Mundial Ramos -> Prezunic Olaria: 5m
+// Mundial Ramos -> Guanabara Bonsucesso: 12m
+// Mundial Ramos -> Mundial Ramos: 0m
+// Mundial Ramos -> Mundial Tijuca: 41m
+// Mundial Ramos -> Extra Copacabana: 51m
+// Mundial Ramos -> Mundial Copacabana: 52m
+// Mundial Ramos -> Assaí São Cristovao: 34m
+// Mundial Ramos -> Atacadão Vicente de Carvalho: 44m
+// Mundial Ramos -> Guanabara Barra da Tijuca: 1h35m
+// [53,40,5,12,0,41,51,52,34,44,95]
+
+
+// Mundial Tijuca -> Casa: 35m
+// Mundial Tijuca -> Guanabara Tijuca (Almirante Cochrane): 8m
+// Mundial Tijuca -> Prezunic Olaria: 46m
+// Mundial Tijuca -> Guanabara Bonsucesso: 49m
+// Mundial Tijuca -> Mundial Ramos: 41m
+// Mundial Tijuca -> Mundial Tijuca: 0m
+// Mundial Tijuca -> Extra Copacabana: 33m
+// Mundial Tijuca -> Mundial Copacabana: 34m
+// Mundial Tijuca -> Assaí São Cristovao: 41m
+// Mundial Tijuca -> Atacadão Vicente de Carvalho: 45m
+// Mundial Tijuca -> Guanabara Barra da Tijuca: 1h18m
+// [35,8,46,49,41,0,33,34,41,45,78]
+
+
+// Extra Copacabana -> Casa: 38m
+// Extra Copacabana -> Guanabara Tijuca (Almirante Cochrane): 58m
+// Extra Copacabana -> Prezunic Olaria: 59m
+// Extra Copacabana -> Guanabara Bonsucesso: 52m
+// Extra Copacabana -> Mundial Ramos: 36m
+// Extra Copacabana -> Mundial Tijuca: 34m
+// Extra Copacabana -> Extra Copacabana : 0m
+// Extra Copacabana -> Mundial Copacabana: 4m
+// Extra Copacabana -> Assaí São Cristovao: 59m
+// Extra Copacabana -> Atacadão Vicente de Carvalho: 52m
+// Extra Copacabana -> Guanabara Barra da Tijuca: 54m
+// [38,58,59,52,36,34,0,4,59,52,54]
+
+// Mundial Copacabana -> Casa: 38m
+// Mundial Copacabana -> Guanabara Tijuca (Almirante Cochrane): 58m
+// Mundial Copacabana -> Prezunic Olaria: 59m
+// Mundial Copacabana -> Guanabara Bonsucesso: 52m
+// Mundial Copacabana -> Mundial Ramos: 36m
+// Mundial Copacabana -> Mundial Tijuca: 34m
+// Mundial Copacabana -> Extra Copacabana: 4m
+// Mundial Copacabana -> Mundial Copacabana: 0m
+// Mundial Copacabana -> Assaí São Cristovao: 59m
+// Mundial Copacabana -> Atacadão Vicente de Carvalho: 52m
+// Mundial Copacabana -> Guanabara Barra da Tijuca: 54m
+// [38,58,59,52,36,34,4,0,59,52,54]
+
+// Assaí São Cristovao -> Casa: 53
+// Assaí São Cristovao -> Guanabara Tijuca (Almirante Cochrane): 48
+// Assaí São Cristovao -> Prezunic Olaria: 49
+// Assaí São Cristovao -> Guanabara Bonsucesso: 30
+// Assaí São Cristovao -> Mundial Ramos: 42
+// Assaí São Cristovao -> Mundial Tijuca: 41
+// Assaí São Cristovao -> Extra Copacabana: 55
+// Assaí São Cristovao -> Mundial Copacabana: 56
+// Assaí São Cristovao -> Assaí São Cristovao: 0
+// Assaí São Cristovao -> Atacadão Vicente de Carvalho: 48
+// Assaí São Cristovao -> Guanabara Barra da Tijuca: 1h31m  
+// [53,48,49,30,42,41,55,56,0,48,91]
+
+// Atacadão Vicente de Carvalho -> Casa: 52
+// Atacadão Vicente de Carvalho -> Guanabara Tijuca (Almirante Cochrane): 40
+// Atacadão Vicente de Carvalho -> Prezunic Olaria: 39
+// Atacadão Vicente de Carvalho -> Guanabara Bonsucesso: 40
+// Atacadão Vicente de Carvalho -> Mundial Ramos: 37
+// Atacadão Vicente de Carvalho -> Mundial Tijuca: 45
+// Atacadão Vicente de Carvalho -> Extra Copacabana: 55
+// Atacadão Vicente de Carvalho -> Mundial Copacabana: 56
+// Atacadão Vicente de Carvalho -> Assaí São Cristovao: 49
+// Atacadão Vicente de Carvalho -> Atacadão Vicente de Carvalho: 0
+// Atacadão Vicente de Carvalho -> Guanabara Barra da Tijuca: 1h40
+// [52,40,39,40,37,45,55,56,49,0,100]
+
+// Guanabara Barra da Tijuca -> Casa: 55
+// Guanabara Barra da Tijuca -> Guanabara Tijuca (Almirante Cochrane): 1h22m
+// Guanabara Barra da Tijuca -> Prezunic Olaria: 1h34m
+// Guanabara Barra da Tijuca -> Guanabara Bonsucesso: 1h35m
+// Guanabara Barra da Tijuca -> Mundial Ramos: 1h32m
+// Guanabara Barra da Tijuca -> Mundial Tijuca: 1h20m
+// Guanabara Barra da Tijuca -> Extra Copacabana: 52m
+// Guanabara Barra da Tijuca -> Mundial Copacabana: 53m
+// Guanabara Barra da Tijuca -> Assaí São Cristovao: 1h24m
+// Guanabara Barra da Tijuca -> Atacadão Vicente de Carvalho: 1h39m
+// Guanabara Barra da Tijuca -> Guanabara Barra da Tijuca: 0
+// [55,82,94,95,92,80,52,53,84,99,0]
+
+
+const maxCost = 480;
+
+const graph = [
+    [0,38,58,59,52,36,4,4,59,52,54],
+    [38,0,43,54,38,4,35,37,47,42,82],
+    [57,42,0,18,5,45,55,56,40,41,103],
+    [60,56,17,0,13,57,58,59,29,42,97],
+    [53,40,5,12,0,41,51,52,34,44,95],
+    [35,8,46,49,41,0,33,34,41,45,78],
+    [38,58,59,52,36,34,0,4,59,52,54],
+    [38,58,59,52,36,34,4,0,59,52,54],
+    [53,48,49,30,42,41,55,56,0,48,91],
+    [52,40,39,40,37,45,55,56,49,0,100],
+    [55,82,94,95,92,80,52,53,84,99,0]
+]
+
+const vertices = [
+    {name: "Casa", minutes: 0}, 
+    {name: "Guanabara Tijuca (Almirante Cochrane)", minutes: 120}, 
+    {name: "Prezunic Olaria", minutes: 60}, 
+    {name: "Guanabara Bonsucesso", minutes: 120}, 
+    {name: "Mundial Ramos", minutes: 60}, 
+    {name: "Mundial Tijuca", minutes: 60}, 
+    {name: "Extra Copacabana", minutes: 60}, 
+    {name: "Mundial Copacabana", minutes: 60}, 
+    {name: "Assaí São Cristóvao", minutes: 60}, 
+    {name: "Atacadão Vicente de Carvalho", minutes: 60}, 
+    {name: "Guanabara Barra da Tijuca", minutes: 120}, 
+]
+
+const getTime = (v) => {
+    return vertices[v].minutes;
+}
+
+const d = (start,goal) => {
+    return graph[start][goal];
+}
+
+const selectNextMove = (current, candidates, start, totalCost) => {
+    let cost = 999;
+    let bestCandidate = -1;
+    for (let candidate of candidates) {
+        const distance = d(current, candidate)
+        const timeCandidate = getTime(candidate);
+        const timeToBack = d(candidate, start);
+    
+        const currentCost = distance + timeCandidate + timeToBack;
+        if (currentCost < cost && (totalCost + distance + timeCandidate) < maxCost) {
+            bestCandidate = candidate;
+            if (current !== start) {
+                // se nao tiver saindo de casa, o tempo de trajeto devera contar no custo total
+                cost = distance + timeCandidate;
+            } else {
+                // se tiver saindo de casa, considerar apenas o tempo da loja no custo total
+                cost = timeCandidate;
+            }
+        }
+    }
+    
+    if (bestCandidate === -1 ){
+        bestCandidate = start;
+        cost = d(current, start);
+    }
+
+    return [bestCandidate, cost];
+}
+
+function findBestRoute(start, goal) {
+    // algoritmo guloso
+    // acha o melhor proximo passo baseado no custo do trajeto + custo do vertice + caminho de volta pra casa
+    // se nao tiver mais tempo, volta pra casa
+    // fazer estilo dijkstra
+    // começa com o inicial
+    // pra cada vizinho, calcula o atual menor custo conehcido e seu ultimo vertice
+
+
+    let current = start;
+    const visited = [start]
+    let totalCost = 0;
+    while (true) {
+        let neighbors = vertices.map((x,i) => i);
+        let candidates = neighbors.filter(neighbor => !visited.includes(neighbor));
+
+        if (!candidates.length) {
+            // totalCost += d(current, start);
+            current = start;
+            visited.push(current);
+            break
+        }
+
+        let [newCurrent, cost] = selectNextMove(current, candidates, start, totalCost)
+
+        console.log("newCurrent", newCurrent, cost)
+
+        totalCost += cost;
+        current = newCurrent;
+        visited.push(current);
+        if (current === start) {
+            break;
+        }
+    }
+
+    console.log("final", visited.map(i => vertices[i].name))
+}
+
+
+findBestRoute(0)
